8982e64e24ca681421eb619bf58a332f6484ce8e
KevGary <kevgary.dev@gmail.com>
2015-11-05 16:11:17 -0700

    UI near completion

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var app = angular.module("app", []);
 app.controller("Controller", function($scope){
-  // $scope.name = "yo, world"
   $scope.postsData = [];
-  $scope.submitForm = function(post){
-    $scope.postsData.push(post);
-    console.log($scope.postsData);
+  $scope.submitForm = function(){
+    $scope.postsData.push(angular.copy($scope.post));
+    $scope.post = {};
   }
 })
