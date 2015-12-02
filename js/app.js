29e064bcf1165744e6772c4d0e4f672393a026fa
KevGary <kevgary.dev@gmail.com>
2015-12-02 12:20:02 -0700

    sending

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var app = angular.module("app", []);
+app.controller("Controller", function($scope){
+  $scope.postsData = [];
+  $scope.submitForm = function(post){
+    post.votes = 0;
+    post.timeSubmitted = moment().calendar();
+    $scope.postsData.push(angular.copy(post));
+    $scope.post = {};
+  }
+  $scope.upVote = function(){
+    this.elem.votes++;
+  }
+  $scope.downVote = function(){
+    this.elem.votes--;
+  }
+
+})
+app.controller("Comments-Controller", function($scope){
+  $scope.commentsData = [];
+  $scope.submitComment = function(comment){
+    $scope.commentsData.push(angular.copy(comment));
+    $scope.comment = {};
+  }
+})
